8894b4beb2c234a88ee403441ad74a4211a31837
zainahda <zainahda@gmail.com>
2022-01-31 22:56:56 +0700

    Niomic tugas 2 Js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+let tugasNumber = 100;
+    tugasString = "Ini adalah string";
+    tugasBoolean = true;
+    tugasNull = null;
+    tugasArray = ["Anggur", "Semangka", "Jambu"];
+
+console.log(tugasNumber);
+console.log(tugasString);
+console.log(tugasBoolean);
+console.log(tugasNull);
+console.log(tugasArray);
